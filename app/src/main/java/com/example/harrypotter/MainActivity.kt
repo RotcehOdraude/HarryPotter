a8408cf42c283662f01f91690965ab36afa7354b
RotcehOdraude <hectoreduardo354@hotmail.com>
2023-06-02 23:02:37 -0600

    En este commit agregé un layout al main activity, por el momento solo dos botones que lanzen ya sea el student o el Staff. Por ahora los layouts de staff y student stan vacios al igual que la logica.

--- app/src/main/java/com/example/harrypotter/MainActivity.kt
+++ app/src/main/java/com/example/harrypotter/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.harrypotter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.harrypotter.databinding.ActivityMainBinding
+import com.example.harrypotter.databinding.ActivityStaffBinding
+import com.example.harrypotter.databinding.ActivityStudentBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
+    fun onClickStaff(view: View) {
+        val intent = Intent(this, Student::class.java)
+        startActivity(intent)
+    }
+    fun onClickStudent(view: View) {
+        val intent = Intent(this, Staff::class.java)
+        startActivity(intent)
+    }
+
 }
